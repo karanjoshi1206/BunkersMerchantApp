dff15dad5709ac82c7bc87359b79ef20784d8411
karanjoshi1206 <joshikaran392@gmail.com>
2022-11-27 22:11:54 +0530

    modal alert removed

--- components/ModalComponent.js
+++ components/ModalComponent.js
@@ -23,7 +23,6 @@ const ModalComponent = ({
 				transparent={true}
 				visible={modalVisible}
 				onRequestClose={() => {
-					Alert.alert("Modal has been closed.");
 					setModalVisible(!modalVisible);
 				}}>
 				<View style={styles.centeredView}>
